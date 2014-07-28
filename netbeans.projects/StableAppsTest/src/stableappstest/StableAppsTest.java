8d277701d3d50804e649625740dfc7f8d10a94f2
jayce1772@live.com <jayce1772@live.com>
2014-07-28 17:49:42 +0800

    another implementation

--- netbeans.projects/StableAppsTest/src/stableappstest/StableAppsTest.java
+++ netbeans.projects/StableAppsTest/src/stableappstest/StableAppsTest.java
@@ -6,8 +6,12 @@
 package stableappstest;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import org.jsoup.Jsoup;
-import org.jsoup.helper.Validate;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
@@ -18,37 +22,59 @@ import org.jsoup.select.Elements;
  */
 public class StableAppsTest {
 
-    public static void main(String[] blalbalba) throws IOException {
+    public static void main(String[] blalbalba) throws IOException, InterruptedException, ExecutionException {
         //Validate.isTrue(args.length == 1, "usage: supply url to fetch");
-        String url = "http://www.visitnorwich.co.uk/";
-        print("Fetching %s...", url);
 
-        Document doc = Jsoup.connect(url).get();
-        Elements links = doc.select("a[href]");
-        Elements media = doc.select("[src]");
-        Elements imports = doc.select("link[href]");
+        Callable<String> c1 = new Callable<String>() {
 
-        print("\nMedia: (%d)", media.size());
-        for (Element src : media) {
-            if (src.tagName().equals("img")) {
-                print(" * %s: <%s> %sx%s (%s)",
-                        src.tagName(), src.attr("abs:src"), src.attr("width"), src.attr("height"),
-                        trim(src.attr("alt"), 20));
-//            } else {
-//                print(" * %s: <%s>", src.tagName(), src.attr("abs:src"));
-//            }
+            @Override
+            public String call() throws Exception {
+                String url = "http://www.huffingtonpost.com/";
+                print("Fetching %s...", url);
+
+                Document doc = Jsoup.connect(url).get();
+                Elements links = doc.select("a[href]");
+                Elements media = doc.select("[src]");
+                Elements imports = doc.select("link[href]");
+
+                print("\nMedia: (%d)", media.size());
+                for (Element src : media) {
+                    if (src.tagName().equals("img")) {
+
+                        System.out.println("tag name: " + src.tagName());
+                        System.out.println("abs:src: " + src.attr("abs:src"));
+                        System.out.println("alt: " + src.attr("alt"));
+                        System.out.println(Thread.currentThread().getName() + "-----");
+                    }
+                }
+
+                return "hello";
             }
-        }
-//
-//        print("\nImports: (%d)", imports.size());
-//        for (Element link : imports) {
-//            print(" * %s <%s> (%s)", link.tagName(),link.attr("abs:href"), link.attr("rel"));
-//        }
-//
-//        print("\nLinks: (%d)", links.size());
-//        for (Element link : links) {
-//            print(" * a: <%s>  (%s)", link.attr("abs:href"), trim(link.text(), 35));
-//        }
+        };
+
+        
+
+
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        Future<String> result = executorService.submit(new Democ());
+        Future<String> r2 = executorService.submit(c1);
+        Future<String> r3 = executorService.submit(c1);
+        Future<String> r4 = executorService.submit(c1);
+        Future<String> r5 = executorService.submit(c1);
+        
+
+        System.out.println(
+                "result: " + r2.get());
+                System.out.println(
+                "result: " + r3.get());
+                        System.out.println(
+                "result: " + r4.get());
+                                System.out.println(
+                "result: " + r5.get());
+                                        System.out.println(
+                "result:!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1 " + result.get());
+               executorService.shutdown();
     }
 
     private static void print(String msg, Object... args) {
@@ -62,10 +88,8 @@ public class StableAppsTest {
             return s;
         }
     }
-    
+
     private static void writeToFileSystem(byte[] fileToWrite, String fileName, String location) {
-    
-    
-    
+
     }
 }
